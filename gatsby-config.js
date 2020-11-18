2ae4db08415ccb67540e801dd9df560ae62f0e45
Marc <marc7806@gmx.de>
2020-11-18 23:10:08 +0100

    Add plugins for markdown and image processing

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,8 +19,33 @@ module.exports = {
     ],
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-transition-link`,
+    // for markdown processing
+    `gatsby-transformer-remark`,
+    // for image processing
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 800,
+            },
+          },
+        ],
+      },
+    },
   ],
 }
